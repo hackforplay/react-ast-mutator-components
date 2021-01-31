f3a89a5e727991f94baaa7323ed0153c1c5b9674
teramotodaiki <i101323@gmail.com>
2021-01-31 21:50:50 +0900

    fix: for test in node

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,4 +1,7 @@
 import { en } from './en';
 import { ja } from './ja';
 
-export const lang = navigator.language.slice(0, 2) === 'ja' ? ja : en;
+export const lang =
+  typeof navigator !== 'undefined' && navigator.language.slice(0, 2) === 'ja'
+    ? ja
+    : en;
